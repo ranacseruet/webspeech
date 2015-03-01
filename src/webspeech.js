04fa6a2d6fdbaa983bf1736ac53b60acfba3b666
nila <rana@EnvyM6-Rana>
2015-03-01 01:14:27 -0500

    throw exception if browser does not support the webspeech api

--- src/webspeech.js
+++ src/webspeech.js
@@ -7,8 +7,12 @@
  */
 function RobotSpeaker()
 {
-    this.u = new SpeechSynthesisUtterance();
-
+	try{
+		this.u = new SpeechSynthesisUtterance();
+	}
+	catch(ex){
+		throw "This browser does not have support for webspeech api";
+	}
     this.u.rate = 1.0;
     this.u.onend = function(event) {
         //TODO
@@ -28,8 +32,14 @@ function RobotSpeaker()
  */
 function AudioListener(callback)
 {
-    this.listener = new webkitSpeechRecognition();
-
+	try{
+		this.listener = new webkitSpeechRecognition();
+	}
+	catch(ex){
+		throw "This browser does not have support for webspeech api";
+		
+	}
+    
     if(platform.os.toString().indexOf("OS X") > -1) {
         this.listener.continuous = true;
     }
