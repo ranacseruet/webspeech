ede84553635ac41a67162e7465200ed8f8f4310a
Rana Md Ali Ahsan <rana@stoplight.io>
2016-01-03 01:39:11 -0500

    - changes the way to create instance. - language variable. - new version.

--- README.md
+++ README.md
@@ -12,19 +12,38 @@ bower install webspeech
 ```
 If you are not using bower, simply download as zip file and include to the HTML page. Also, don't forget to include the [platform.js](https://github.com/bestiejs/platform.js) file as well.(in case of bower, it will be installed automatically as dependency)
 
+```
+<script src="https://raw.githubusercontent.com/ranacseruet/webspeech/master/build/webspeech.min.js"></script>
+```
+
 ## Usage
 
 ```
 //Text To Voice
-    var speaker = new RobotSpeaker();
+    var speaker = new webspeech.Speaker();
     speaker.speak("en", document.getElementById("text").value);
 
 //Voice To Text
-    var listener = new AudioListener();
+    var listener = new webspeech.Listener();
     listener.listen("en", function(text) {
         document.getElementById("text").value = text;
     });
 ```
+
+##Language Compatibility
+There isn't any official list of languages which are supported in web speech api. However, I believe, the languages listed on the
+[demonstration page](https://www.google.com/intl/en/chrome/demos/speech.html) is suppose to be a legitimate list. For any languages,
+you should use the [language ISO codes](https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes) and you should be fine.
+
+Library includes a language variable(in progress, need to add languages)
+that you can use like follows:
+
+```
+//webspeech.languages.ENGLISH
+or
+//webspeech.languages.FRENCH
+```
+
 
 ## Development
 You must have to have node/npm and grunt installed on your development machine.
--- src/webspeech.js
+++ src/webspeech.js
@@ -1,99 +1,115 @@
-/**
- * Created by Rana on 14-12-20.
- */
-
-/**
- * a robotic speaker who speaks with given text and language
- */
-function RobotSpeaker()
-{
-	try{
-		this.u = new SpeechSynthesisUtterance();
-	}
-	catch(ex){
-		throw "This browser does not have support for webspeech api";
-	}
-    this.u.rate = 1.0;
-    this.u.onend = function(event) {
-        //TODO
-    };
-    this.speak = function(lang, text){
-        this.u.lang = lang;
-        this.u.text = text;
-        speechSynthesis.speak(this.u);
-    };
-}
-
-/**
- * An audio listener which will listen and convert spoken sounds to text
- * @param lang
- * @param callback
- * @constructor
- */
-function AudioListener(callback)
-{
-	try{
-		this.listener = new webkitSpeechRecognition();
-	}
-	catch(ex){
-		throw "This browser does not have support for webspeech api";
-		
-	}
-    
-    if(platform.os.toString().indexOf("OS X") > -1) {
-        this.listener.continuous = true;
-    }
-    if(callback) {
-        this.callBack = callback;
-    }
-    this.listener.lang = "en";
-    //this.listener.interimResults = true;
-    var me = this;
-
-    //-----Events---------
-    this.listener.onresult = function(event) {
-        if (event.results.length > 0) {
-            var result = event.results[event.results.length-1];
-            if(result.isFinal) {
-                me.callBack(result[0].transcript);
-            }
+(function () {
+    /**
+     * a robotic speaker who speaks with given text and language
+     */
+    function RobotSpeaker()
+    {
+        try{
+            this.u = new SpeechSynthesisUtterance();
         }
-    };
-    this.listener.onsoundstart = function(event) {
-        //TODO
-    };
-    this.listener.onspeechstart = function(event) {
-        //TODO
-    };
-    this.listener.onsoundend = function(event) {
-       //TODO
-    };
-    //-----End Events---------
-
-    this.isContinuous = function() {
-        return this.listener.continuous;
-    };
+        catch(ex){
+            throw "This browser does not have support for webspeech api";
+        }
+        this.u.rate = 1.0;
+        this.u.onend = function(event) {
+            //TODO
+        };
+        this.speak = function(lang, text){
+            this.u.lang = lang;
+            this.u.text = text;
+            speechSynthesis.speak(this.u);
+        };
+    }
 
     /**
-     * Start listening with given language(optional, defaults is english)
-     * @param lang
+     * An audio listener which will listen and convert spoken sounds to text
+     * @param callback function to call when a audio is listened and recognized
+     * @constructor
      */
-    this.listen = function(lang, callback) {
-        if(lang) {
-            this.listener.lang = lang;
+    function AudioListener(callback)
+    {
+        try{
+            this.listener = new webkitSpeechRecognition();
+        }
+        catch(ex){
+            throw "This browser does not have support for webspeech api";
+        }
+
+        if(platform.os.toString().indexOf("OS X") > -1) {
+            //Seems like, on windows, continuous listening doesn't work/supported
+            this.listener.continuous = true;
         }
         if(callback) {
             this.callBack = callback;
         }
-        this.listener.start();
-    };
+        this.listener.lang = "en";
 
-    /**
-     * Stop listening
-     * @param lang
-     */
-    this.stop = function() {
-        this.listener.stop();
-        console.log("audio listener stopped");
+        //TODO work with less accurate results, which are returned very fast
+        //this.listener.interimResults = true;
+
+        var me = this;
+
+        //-----Events---------
+        this.listener.onresult = function(event) {
+            if (event.results.length > 0) {
+                var result = event.results[event.results.length-1];
+                if(result.isFinal) {
+                    me.callBack(result[0].transcript);
+                }
+            }
+        };
+        this.listener.onsoundstart = function(event) {
+            //TODO
+        };
+        this.listener.onspeechstart = function(event) {
+            //TODO
+        };
+        this.listener.onsoundend = function(event) {
+            //TODO
+        };
+        //-----End Events---------
+
+        this.isContinuous = function() {
+            return this.listener.continuous;
+        };
+
+        /**
+         * Start listening with given language(optional, defaults is english)
+         * @param lang
+         */
+        this.listen = function(lang, callback) {
+            if(lang) {
+                this.listener.lang = lang;
+            }
+            if(callback) {
+                this.callBack = callback;
+            }
+            this.listener.start();
+        };
+
+        /**
+         * Stop listening
+         * @param lang
+         */
+        this.stop = function() {
+            this.listener.stop();
+            console.log("audio listener stopped");
+        };
+    }
+
+
+    var ws = {
+        Speaker: RobotSpeaker,
+        Listener: AudioListener,
+        Languages: {
+            ENGLISH: "en",
+            FRENCH: "fr"
+        }
     };
-}
+    if (!window.webspeech) {
+        webspeech = ws;
+    }
+    webSpeechNoConflict = function() {
+        return ws;
+    };
+})();
